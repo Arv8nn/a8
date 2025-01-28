721b3d62a07ecccc1cf2790f4377c2ba2fe44b66
ambe2222 <arv8test@mailum.com>
2025-01-29 02:11:55 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 echo "=================================================================="
 echo "=================================================================="
-echo "serv00 اسکریپت ترجمه شده نصب سینگ باکس بر روی هاست"
-echo "translated by ARV8n"
-echo "suorce = yonggekkk"
-echo "برای نصب از ای پی های موجود در اسکریپت استفاده کنید"
-echo "با زدن اینتر در مراحل بعد مقادیر پیش فرض ست خواهند شد"
+echo "made and translated by ARV8n"
+echo "suorce = yonggekkk , mtg"
 echo "Please select an option:"
 echo "==================================================================="
 echo "==================================================================="
 
-select option in "install script" "exit"
+select option in "install script(translate from yonggekkk)" "serv00 MTproxy" "exit"
 do
     case $option in
 	"install script")
-	echo "you selected install script"
+	echo "you selected install script(translate from yonggekkk)"
 	bash <(curl -Ls https://raw.githubusercontent.com/ambe2222/a8/refs/heads/main/files/serv00.sh)
 	;;
+ 	"serv00 MTproxy")
+  	echo "you selected serv00 MTproxy"
+   	bash <(curl -Ls https://raw.githubusercontent.com/ambe2222/a8/refs/heads/main/mt.sh)
+    	;;
 	"exit")
 	echo "Exiting the program"
             break
