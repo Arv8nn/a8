c6ce657d01ad347a7acbeea59607cad708694be8
ambe2222 <arv8test@mailum.com>
2025-01-28 21:35:10 +0330

    Add files via upload

--- mt.sh
+++ mt.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
 
-# مشخص کردن URL فایل برای دانلود
+echo "serv00 MTproxy for telegram by ARV8N"
+echo "serv00 MTproxy for telegram by ARV8N"
+echo "serv00 MTproxy for telegram by ARV8N"
+echo "serv00 MTproxy for telegram by ARV8N"
+echo "serv00 MTproxy for telegram by ARV8N"
+echo "serv00 MTproxy for telegram by ARV8N"
+echo "serv00 MTproxy for telegram by ARV8N"
 FILE_URL="https://github.com/9seconds/mtg/releases/download/v2.1.7/mtg-2.1.7-freebsd-amd64.tar.gz"
 DIR_NAME="mtg-2.1.7-freebsd-amd64"
 
-# دانلود فایل و استخراج آن
-echo "در حال دانلود فایل mtg..."
+
+echo "downloading mtg file..."
 wget -q $FILE_URL -O mtg.tar.gz
 
-echo "در حال استخراج فایل..."
+echo "extracting..."
 tar -xzf mtg.tar.gz
 cd mtg-2.1.7-freebsd-amd64
 
-# درخواست از کاربر برای وارد کردن نام هاست و پورت
-read -p "لطفا نام هاست را وارد کنید: " host
-read -p "لطفا پورت را وارد کنید: " port
 
-# تولید کلید مخفی با استفاده از نام هاست وارد شده
+read -p "enter your host name(ex:s1.serv00.com): " host
+read -p "enter your port(1025-60000): " port
+
+
 secret=$(./mtg generate-secret --hex $host)
-# ساخت لینک تلگرام با استفاده از هاست و پورت وارد شده و کلید مخفی تولید شده
+
 mtproto_url="https://t.me/proxy?server=${host}&port=${port}&secret=${secret}"
 
-# نمایش لینک تلگرام به کاربر
-echo "پروکسی با موفقیت راه‌اندازی شد!"
-echo "برای دسترسی به پروکسی، از لینک زیر استفاده کنید:"
+#
+echo "proxy online!"
+echo "use this link to accsess proxy in telegram"
 echo "$mtproto_url"
-
-# اجرای دستور mtg برای راه‌اندازی پروکسی با استفاده از کلید مخفی و پورت وارد شده
 echo "wait"
+echo "made by ARV8N"
+echo "have fun"
 nohup ./mtg simple-run -n 1.1.1.1 -t 30s -a 1MB 0.0.0.0:${port} ${secret} -c 8192 > mtg.log 2>&1 &
+
