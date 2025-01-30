ca41137c03539c453a2181d7660637258ee911a8
ambe2222 <arv8test@mailum.com>
2025-01-30 11:27:17 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,21 +12,21 @@ select option in "yonggekkk serv00 script" "eooce serv00 script" "serv00 socks5"
 do
     case $option in
 	"yonggekkk serv00 script")
-	echo "yonggekkk serv00 script"
+	echo "yonggekkk serv00 script (vless , vmess , hy2 , tuic)"
 	bash <(curl -Ls https://raw.githubusercontent.com/ambe2222/a8/refs/heads/main/serv%201/serv00.sh)
 	;;
  	"eooce serv00 script")
-  	echo "eooce serv00 script"
+  	echo "eooce serv00 script (vless , vmess , hy2 , tuic)"
    	bash <(curl -Ls https://raw.githubusercontent.com/ambe2222/a8/refs/heads/main/serv%202/sb_serv00.sh)
     	;;
-     	"serv00 MTproxy")
-      	echo "serv00 MTproxy"
-       	bash <(curl -Ls https://raw.githubusercontent.com/ambe2222/a8/refs/heads/main/mt.sh)
-	;;
  	"serv00 socks5")
   	echo "serv00 socks5"
    	bash <(curl -Ls https://raw.githubusercontent.com/ambe2222/a8/refs/heads/main/socks5/install-socks5.sh)
     	;;
+        "serv00 MTproxy")
+      	echo "serv00 MTproxy"
+       	bash <(curl -Ls https://raw.githubusercontent.com/ambe2222/a8/refs/heads/main/mt.sh)
+	;;
 	"exit")
 	echo "Exiting the program"
             break
