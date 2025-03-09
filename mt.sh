cfd58a22b4e65b343ab44823fa018b2e562cfeea
ambe2222 <arv8test@mailum.com>
2025-03-09 22:29:43 +0330

    Add files via upload

--- mt.sh
+++ mt.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/bin/env bash
+tput bold;tput setaf 2
+echo '
+____ ____ ____ _  _    _  _ ___ ___  ____ ____ _  _ _   _    ____ ____ ____
+[__  |___ |__/ |  |    |\/|  |  |__] |__/ |  |  \/   \_/     |___ |  | |__/
+___] |___ |  \  \/     |  |  |  |    |  \ |__| _/\_   |      |    |__| |  \
+                                                                           
+___ ____ _    ____ ____ ____ ____ _  _    ___  _   _    ____ ____ _  _ _  _
+ |  |___ |    |___ | __ |__/ |__| |\/|    |__]  \_/     |__| |__/ |  | |\ |
+ |  |___ |___ |___ |__] |  \ |  | |  |    |__]   |      |  | |  \  \/  | \|
+'
+tput sgr0
 
-echo "serv00 MTproxy for telegram by ARV8N"
-echo "serv00 MTproxy for telegram by ARV8N"
-echo "serv00 MTproxy for telegram by ARV8N"
-echo "serv00 MTproxy for telegram by ARV8N"
-echo "serv00 MTproxy for telegram by ARV8N"
-echo "serv00 MTproxy for telegram by ARV8N"
-echo "serv00 MTproxy for telegram by ARV8N"
 FILE_URL="https://github.com/9seconds/mtg/releases/download/v2.1.7/mtg-2.1.7-freebsd-amd64.tar.gz"
 DIR_NAME="mtg-2.1.7-freebsd-amd64"
 
@@ -27,12 +31,17 @@ secret=$(./mtg generate-secret --hex $host)
 
 mtproto_url="https://t.me/proxy?server=${host}&port=${port}&secret=${secret}"
 
-#
+tput bold; tput setaf 2
 echo "proxy online!"
 echo "use this link to accsess proxy in telegram"
+tput sgr0
 echo "$mtproto_url"
+tput bold; tput setaf 3
 echo "wait"
+tput sgr0
+tput bold; tput setaf 2
 echo "made by ARV8N"
 echo "have fun"
+tput sgr0
 nohup ./mtg simple-run -n 1.1.1.1 -t 30s -a 1MB 0.0.0.0:${port} ${secret} -c 8192 > mtg.log 2>&1 &
 
--- start.sh
+++ start.sh
@@ -1,49 +1,84 @@
-#!/bin/bash
-
-clear
-echo "==================================================================="
-echo "==================================================================="
-echo "made and translated by ARV8n"
-echo "source = yonggekkk , eooce , mtg , cmliu"
-echo "Please select an option:"
-echo "==================================================================="
-echo "==================================================================="
+#!/bin/env bash
 
 yonggekkk_script="https://raw.githubusercontent.com/ambe2222/a8/refs/heads/main/serv%201/serv00.sh"
 eooce_script="https://raw.githubusercontent.com/ambe2222/a8/refs/heads/main/serv%202/sb_serv00.sh"
 socks5_script="https://raw.githubusercontent.com/ambe2222/a8/refs/heads/main/socks5/install-socks5.sh"
 mtproxy_script="https://raw.githubusercontent.com/ambe2222/a8/refs/heads/main/mt.sh"
 
-select option in \
-    "yonggekkk serv00 script (vless , vmess , hy2 , tuic)" \
-    "eooce serv00 script (vless , vmess , hy2 , tuic)" \
-    "serv00 socks5" \
-    "serv00 MTproxy" \
-    "exit"
-do
+function yonggekkk () {
+    clear
+    echo "$(tput setaf 2)runnig yonggekkk script$(tput sgr0)"
+    bash <(curl -Ls "$yonggekkk_script") || echo "Error: Failed to execute script."
+}
+
+function eooce () {
+    clear
+    echo "$(tput setaf 2)runnig eooce script$(tput sgr0)"
+    bash <(curl -Ls "$eooce_script") || echo "Error: Failed to execute script."
+}
+
+function socks5 () {
+    clear
+    echo "$(tput setaf 2)runnig serv00 socks5 script$(tput sgr0)"
+    bash <(curl -Ls "$socks5_script") || echo "Error: Failed to execute script."
+}
+
+function MTproxy () {
+    clear
+    echo "$(tput setaf 2)runnig serv00 MTproxy script$(tput sgr0)"
+    bash <(curl -Ls "$mtproxy_script") || echo "Error: Failed to execute script."
+}
+
+function menu () {
+    clear
+    echo "$(tput bold)this script made by :"
+    tput sgr0
+    tput bold;tput blink;tput setaf 6
+    echo '           _______      _____  _   _ 
+        /\   |  __ \ \    / / _ \| \ | |
+       /  \  | |__) \ \  / / (_) |  \| |
+      / /\ \ |  _  / \ \/ / > _ <| . ` |
+     / ____ \| | \ \  \  / | (_) | |\  |
+    /_/    \_\_|  \_\  \/   \___/|_| \_|
+    '
+    tput sgr0
+    echo "$(tput bold)welcome $(tput setaf 2)`whoami`"
+    tput sgr0
+    echo "==================================================================="
+    echo "==================================================================="
+    tput bold
+    echo -e "\ttranslated by $(tput setaf 2)ARV8N$(tput sgr0)"
+    tput bold
+    echo -e "\t$(tput setaf 4)source$(tput sgr0) $(tput bold)= yonggekkk , eooce , mtg , cmliu"
+    tput sgr0
+    echo "==================================================================="
+    echo "==================================================================="
+    tput setaf 2
+    echo -e "\t\t***** please select an option *****"
+    tput sgr0
+    tput setaf 3
+    echo -e "\t1. yonggekkk serv00 script (vless , vmess , hy2 , tuic)\n"
+    echo -e "\t2. eooce serv00 script (vless , vmess , hy2 , tuic)"
+    tput setaf 1
+    echo -e "\t(this script has problems whit other language, so i just translate the menu"
+    echo -e "\tin next steps just press enter)"
+    echo -e "\tfor uninstall select: 3\n"
+    tput setaf 3 
+    echo -e "\t3. serv00 socks5\n"
+    echo -e "\t4. serv00 MTproxy\n"
+    echo -e "\t5. Exit\n"
+    read -p "select what you want [1-5] : " option
+    tput sgr0
+}
+while true; do
+    menu
+    echo ""
     case $option in
-        "yonggekkk serv00 script (vless , vmess , hy2 , tuic)")
-            echo "Running yonggekkk script..."
-            bash <(curl -Ls "$yonggekkk_script") || echo "Error: Failed to execute script."
-            ;;
-        "eooce serv00 script (vless , vmess , hy2 , tuic)")
-            echo "Running eooce script..."
-            bash <(curl -Ls "$eooce_script") || echo "Error: Failed to execute script."
-            ;;
-        "serv00 socks5")
-            echo "Installing socks5..."
-            bash <(curl -Ls "$socks5_script") || echo "Error: Failed to execute script."
-            ;;
-        "serv00 MTproxy")
-            echo "Installing MTproxy..."
-            bash <(curl -Ls "$mtproxy_script") || echo "Error: Failed to execute script."
-            ;;
-        "exit")
-            echo "Exiting the program"
-            break
-            ;;
-        *)
-            echo "Invalid option. Please try again."
-            ;;
+        1) yonggekkk ;;
+        2) eooce ;;
+        3) socks5 ;;
+        4) MTproxy ;;
+        5) exit ;;
+        *) echo "choose a valid option [5 for Exit]"
     esac
 done
